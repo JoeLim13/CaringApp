b3a5d5259849408246223a7ff606d0f87c479668
Iwan-G <74765961+Iwan-G@users.noreply.github.com>
2021-02-07 14:58:40 +0800

    Added User api

--- caringapp_web/blueprints/users/views.py
+++ caringapp_web/blueprints/users/views.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+import peewee as pw
+from models.user import User
+from werkzeug import secure_filename
+from caringapp_web.util.helpers import upload_file_to_s3
+from flask_login import login_required, login_user, current_user
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 
 users_blueprint = Blueprint('users',
@@ -6,31 +11,116 @@ users_blueprint = Blueprint('users',
                             template_folder='templates')
 
 
-@users_blueprint.route('/new', methods=['GET'])
+@users_blueprint.route('/new', methods = ['GET'])
 def new():
     return render_template('users/new.html')
 
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    params = request.form
+
+    new_user = User(username = params.get("username"), email = params.get("email"), password = params.get("password"))
+
+    if new_user.save():
+        flash("Successfully Signed Up")
+        login_user(new_user)
+        return redirect(url_for("users.show", username = new_user.username))
+    else:
+        for err in new_user.errors:
+            flash(err, "danger")
+        return redirect(url_for("users.new"))
 
 
-@users_blueprint.route('/<username>', methods=["GET"])
+@users_blueprint.route('/<username>', methods = ["GET"])
+@login_required
 def show(username):
-    pass
+    user = User.get_or_none(User.username == username)
+    if user:
+        user = pw.prefetch(user)
+        return render_template("users/show.html", user = user)
+    else:
+        flash(f"No {username} user found", "danger")
+        return redirect(url_for('home'))
 
 
-@users_blueprint.route('/', methods=["GET"])
+@users_blueprint.route('/', methods = ["GET"])
 def index():
     return "USERS"
 
 
-@users_blueprint.route('/<id>/edit', methods=['GET'])
+@users_blueprint.route('/<id>/edit', methods = ['GET'])
+@login_required
 def edit(id):
-    pass
+    user = User.get_or_none(User.id == id)
+    if user:
+        if current_user.id == int(id):
+            return render_template("users/edit.html", user = user)
+        else:
+            flash("Cannot edit someone else's profile")
+            return redirect(url_for('users.show', username = user.username))
+    else:
+        flash("No user found")
+        return redirect(url_for("home"))
 
 
-@users_blueprint.route('/<id>', methods=['POST'])
+@users_blueprint.route('/<id>', methods = ['POST'])
 def update(id):
-    pass
+    user = User.get_or_none(User.id == id)
+    if user:
+        if current_user.id == int(id):
+            params = request.form
+        
+            user.username = params.get("username")
+            user.email = params.get("email")
+            password = params.get("password")
+
+            if len(password) > 0:
+                user.password = password
+
+            if user.save():
+                flash("Successfully updated details.")
+                return redirect(url_for("users.show"), username = user.username)
+            else:
+                flash("Failed to edit your profile. Try again")
+                for err in user.errors:
+                    flash(err)
+                return redirect(url_for("users.edit", id = user.id))
+        else:
+            flash("You cannot edit details of another user")
+            return redirect(url_for("users.show", username = user.username))
+    else:
+        flash("No such user!")
+        redirect(url_for("home"))
+
+@users_blueprint.route('/<id>/upload', methods=['POST'])
+@login_required
+def upload(id):
+    user = User.get_or_none(User.id == id)
+    if user:
+        if current_user.id == int(id):
+           
+            if "profile_image" not in request.files:
+                flash("No file provided!")
+                return redirect(url_for("users.edit", id=id))
+
+            file = request.files["profile_image"]
+
+            file.filename = secure_filename(file.filename)
+            
+            image_path = upload_file_to_s3(file,user.username )
+            
+            user.image_path = image_path
+
+            print(user.image_path)
+            if user.save():
+                return redirect(url_for("users.show", username=user.username))
+            else:
+                flash("Could not upload image. Please try again")
+                return redirect(url_for("users.edit", id=id))       
+        else:
+            flash("Cannot edit users other than yourself!")
+            return redirect(url_for("users.show", username=user.username))
+    else:
+        flash("No such user!")
+        redirect(url_for("home"))
