368738c5ab8c3bffe5bce64edb6dd0f7a7ff58b2
Joe Lim <joecounterzero@hotmail.com>
2021-02-08 21:41:51 +0800

    edited task scheduler

--- caringapp_web/blueprints/users/views.py
+++ caringapp_web/blueprints/users/views.py
@@ -156,29 +156,40 @@ def upload(id):
         flash("No such user!")
         redirect(url_for("home"))
 
-@scheduler.task('cron', id='do_save_to_dashboard', hour='18', minute='30', second='0')
+@scheduler.task('cron', id='do_save_to_dashboard', hour='00', minute='00', second='0')
 def save_to_dashboard():
-    user = User.get_or_none(User.username == username)
-    if user:
-        current_day = date.today()
-        previous_day = Activity.select().where((Activity.completion_date < current_day)).order_by(Activity.completion_date.desc()).limit(1)
-        for p in previous_day:
-            print(p.completion_date)
-        yesterday = p.completion_date
-        if yesterday:
-            from models.dailyrecord import DailyRecord
-            is_completed = Activity.select().where(Activity.is_completed == 1, Activity.completion_date == yesterday).count()
-            print(is_completed)
-            task = Activity.select().where(Activity.completion_date == yesterday).count()
-            print(task)
-            completion_rate = is_completed / task * 100
-            title = yesterday
-            relationship = DailyRecord(title = title, completion_rate = completion_rate, user = user.id)
-            if relationship.save() :
-                print('Successfully saved')
-            else :
-                print('Something wrong in the code')
+    all_user = User.select()
+    for user in all_user:
+        print(user.username)
+        if user:
+            current_day = date.today()
+            previous_day = Activity.select().where((Activity.completion_date < current_day)).order_by(Activity.completion_date.desc()).limit(1)
+            for p in previous_day:
+                print(p.completion_date)
+            yesterday = p.completion_date
+            if yesterday:
+                from models.dailyrecord import DailyRecord
+                is_completed = Activity.select().where(Activity.is_completed == 1, Activity.completion_date == yesterday, Activity.user == user.id).count()
+                print(is_completed)
+                task = Activity.select().where(Activity.completion_date == yesterday, Activity.user == user.id).count()
+                if task == 0:
+                    print(user.id)
+                    print(task)
+                    completion_rate = 0
+                    title = yesterday
+                    relationship = DailyRecord(title = title, completion_rate = completion_rate, user = user.id)
+                else:
+                    print(user.id)
+                    print(task)
+                    completion_rate = is_completed / task * 100
+                    title = yesterday
+                    relationship = DailyRecord(title = title, completion_rate = completion_rate, user = user.id)
+                if relationship.save() :
+                    print('Successfully saved')
+                else :
+                    print('Something wrong in the code')
+            else:
+                print('Query Problem')
         else:
-            print('Query Problem')
-    else:
-        print('No such user')  
+            print('No such user')
+    print("done")
